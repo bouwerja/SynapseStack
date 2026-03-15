632ebf1675a2f4d14393032448fa68aff1ac2b54
JasonBouwer <bouwerjason1@gmail.com>
2026-03-15 11:15:02 +0200

    working register scrapper

--- backend/scrapper/register.go
+++ backend/scrapper/register.go
@@ -1,37 +1,79 @@
 package scrapper
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
 	"strings"
+	"sync"
+	"time"
 
-	"github.com/gocolly/colly"
-	// "github.com/gocolly/colly/v2"
+	// "github.com/gocolly/colly"
+	"github.com/gocolly/colly/v2"
 )
 
 type Business struct {
-	Cateogry    string
-	CompanyName string
-	Location    string
+	Category    string `json:"operating_sector"`
+	CompanyName string `json:"company_name"`
+	Location    string `json:"company_location"`
 }
 
 func BusinessRegisterScrapper() {
-	/*
-		fName := "/JSONdata/business.json"
+	var mu sync.Mutex
 
-		file, err := os.Create(fName)
-		if err != nil {
-			log.Fatalf("Cannot create file %q: %s\n", fName, err)
-		}
-		defer file.Close()
-	*/
+	fName := "scrapper/JSONdata/business.json"
+
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+	}
+	defer file.Close()
+
+	var businesses []Business
 
 	c := colly.NewCollector(
 		colly.AllowedDomains("brabys.com", "www.brabys.com"),
+		colly.Async(true),
 	)
 
+	c.WithTransport(&http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		ForceAttemptHTTP2: true,
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
+		r.Headers.Set("Accept-Language", "en-US,en;q=0.9")
+		r.Headers.Set("Referer", "https://www.google.com/")
+		r.Headers.Set("Sec-Ch-Ua", `"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"`)
+		r.Headers.Set("Sec-Fetch-Mode", "navigate")
+	})
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*brabys.com",
+		Parallelism: 5,
+		RandomDelay: 1 * time.Second,
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("ERROR on %s: Status %d | %v\n", r.Request.URL, r.StatusCode, err)
+	})
+
 	c.OnHTML("#category-verified-business a.sub-category", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		fmt.Printf("Found Category: %s\n", e.Text)
+		categoryName := strings.TrimSpace(e.Text)
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		// link := e.Attr("href")
+		// fmt.Printf("Found Category: %s\n", e.Text)
+
+		ctx := colly.NewContext()
+		ctx.Put("category", categoryName)
 
 		e.Request.Visit(link)
 	})
@@ -39,21 +81,21 @@ func BusinessRegisterScrapper() {
 	c.OnHTML("div.grid_element", func(e *colly.HTMLElement) {
 		bizName := e.ChildAttr("a", "title")
 		bizName = strings.Split(bizName, " - Business")[0]
-		fmt.Printf("---- Scraping Business: %s ----\n", bizName)
-
-		/*
-			locationContainer := e.DOM.Find("span.member-search-location small")
-			bizArea := locationContainer.Contents().First().Text()
-			bizArea = strings.Split(strings.TrimSpace(bizArea), ",")[0]
-			bizRegion := e.ChildText("span.inline-block")
-			fmt.Println("Business Location: ")
-			fmt.Printf("%s %s\n", bizArea, bizRegion)
-		*/
 
 		rawLocation := e.ChildText("span.member-search-location small")
 		cleanLocation := strings.Join(strings.Fields(rawLocation), " ")
-		fmt.Println("Business Locations: ")
-		fmt.Println(cleanLocation)
+
+		category := e.Response.Ctx.Get("category")
+
+		item := Business{
+			Category:    category,
+			CompanyName: bizName,
+			Location:    cleanLocation,
+		}
+
+		mu.Lock()
+		businesses = append(businesses, item)
+		mu.Unlock()
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -61,4 +103,21 @@ func BusinessRegisterScrapper() {
 	})
 
 	c.Visit("https://www.brabys.com/categories/")
+	c.Wait()
+
+	writeJSON(fName, businesses)
+}
+
+func writeJSON(fileName string, data []Business) {
+	file, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		log.Fatalf("error marshalling: %s", err)
+	}
+
+	err = os.WriteFile(fileName, file, 0o644)
+	if err != nil {
+		log.Fatalf("error writing file: ", err)
+	}
+
+	fmt.Printf("\nSuccessfully saved %d buinsesses to %s\n", len(data), fileName)
 }
